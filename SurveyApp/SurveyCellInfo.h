6e8cf3fe921269306287a2d00fd69d37aadfb4fa
Binh Le <binh@keaz.co>
2016-10-25 10:33:14 +0700

    Remove notification when dealloc

--- SurveyApp/SurveyCellInfo.h
+++ SurveyApp/SurveyCellInfo.h
@@ -6,5 +6,10 @@
 @property (weak, nonatomic) IBOutlet UILabel *lblName;
 @property (weak, nonatomic) IBOutlet UITextView *tvDescription;
 @property (weak, nonatomic) IBOutlet UIImageView *imvHotel;
+/**
+ *  Set data for cell
+ *
+ *  @param hotelInfo hotelInfo
+ */
 -(void)setDataForCell:(SAHotelInfo *)hotelInfo;
 @end
